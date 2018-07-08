3206af75ba1da44e2a6b274b9bf01bc6e3f1bd82
roshan2M <r2munjal@edu.uwaterloo.ca>
2018-07-08 19:46:40 -0400

    Starting week 1 on graph algorithms.

---
+++ src/week_1/lectures/undirected_graphs/DepthFirstPaths.java
@@ -0,0 +1,13 @@
+package week_1.lectures.undirected_graphs;
+
+public class DepthFirstPaths {
+
+	private boolean[] visited;
+	private int[] edgeTo;
+	private int vertex;
+
+	public DepthFirstPaths(Graph g, int s) {
+
+	}
+
+}
