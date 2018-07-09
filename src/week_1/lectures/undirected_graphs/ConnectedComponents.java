d297bd08107d6de33785c7f8e9dafa17a7b7692a
roshan2M <r2munjal@edu.uwaterloo.ca>
2018-07-08 22:35:55 -0400

    Find connected components.

--- src/week_1/lectures/undirected_graphs/ConnectedComponents.java
+++ src/week_1/lectures/undirected_graphs/ConnectedComponents.java
@@ -1,4 +1,52 @@
 package week_1.lectures.undirected_graphs;
 
+import com.google.common.graph.Graph;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ConnectedComponents {
+
+	private List<List<Integer>> connectedComponents;
+	private boolean[] visited;
+
+	ConnectedComponents(Graph G) {
+		visited = new boolean[G.edges().size()];
+		connectedComponents = new ArrayList<>();
+		for (Object v : G.edges()) {
+			if (!visited[(int) v]) {
+				addConnectedComponent(G, (int) v);
+			}
+		}
+	}
+
+	private void addConnectedComponent(Graph G, int v) {
+		List<Integer> component = new ArrayList<>();
+		DepthFirstPaths searchNode = new DepthFirstPaths(G, v);
+		boolean[] visitedNodes = searchNode.visited();
+		for (int i = 0; i < visitedNodes.length; i++) {
+			if (visitedNodes[i]) {
+				component.add(i);
+			}
+		}
+		connectedComponents.add(component);
+	}
+
+	public boolean connected(int v, int w) {
+		for (List<Integer> component : connectedComponents){
+			if (component.contains(v) && component.contains(w)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public List<List<Integer>> connectedComponents() {
+		return this.connectedComponents;
+	}
+
+	public boolean[] visited() {
+		return this.visited;
+	}
+
 }
