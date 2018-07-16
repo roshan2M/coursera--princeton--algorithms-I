cce1a334e016d462c5513cfe7eb22032beec4205
roshan2M <r2munjal@edu.uwaterloo.ca>
2018-07-16 11:24:55 -0600

    Start Wordnet API.

---
+++ src/week_1/problem_set/wordnet/WordNet.java
@@ -0,0 +1,32 @@
+package week_1.problem_set.wordnet;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class WordNet {
+
+	public WordNet(String synsets, String hypernyms) {
+
+	}
+
+	public List<String> nouns() {
+		return new ArrayList<String>();
+	}
+
+	public boolean isNoun(String word) {
+		return true;
+	}
+
+	public int distance(String nounA, String nounB) {
+		return 0;
+	}
+
+	public String sap(String nounA, String nounB) {
+		return "";
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
