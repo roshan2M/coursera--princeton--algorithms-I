0a547db0e5c1c6df3563c4377e1b468b4eec6680
roshan2M <r2munjal@edu.uwaterloo.ca>
2018-07-29 18:04:03 -0400

    Add topological sort for directed graphs.

---
+++ src/week_1/lectures/directed_graphs/TopologicalSort.java
@@ -0,0 +1,41 @@
+package week_1.lectures.directed_graphs;
+
+import com.google.common.graph.MutableGraph;
+
+import java.util.Stack;
+
+public class TopologicalSort {
+
+	private boolean[] visited;
+	private Stack<Integer> reversePost;
+
+	public TopologicalSort(MutableGraph G) {
+		visited = new boolean[G.nodes().size()];
+		reversePost = new Stack<>();
+
+		for (Object i : G.nodes()) {
+			if (!visited[(int) i]) {
+				dfs(G, (int) i);
+			}
+		}
+	}
+
+	private void dfs(MutableGraph G, int v) {
+		visited[v] = true;
+		for (Object i : G.adjacentNodes(v)) {
+			if (!visited[(int) i]) {
+				dfs(G, (int) i);
+			}
+		}
+		reversePost.push(v);
+	}
+
+	public boolean[] visited() {
+		return visited;
+	}
+
+	public Stack<Integer> reversePost() {
+		return reversePost;
+	}
+
+}
