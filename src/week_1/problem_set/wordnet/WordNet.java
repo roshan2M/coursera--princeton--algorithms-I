869cac8277545c9ffb861bd457e72341831776d3
roshan2M <r2munjal@edu.uwaterloo.ca>
2018-08-05 13:13:21 -0400

    WordNet class to build and use the WordNet digraph.

--- src/week_1/problem_set/wordnet/WordNet.java
+++ src/week_1/problem_set/wordnet/WordNet.java
@@ -1,32 +1,112 @@
 package week_1.problem_set.wordnet;
 
-import java.util.List;
+/*********
+ *
+ * @author Roshan Munjal
+ *
+ * WordNet Module API.
+ *
+ *********/
+
 import java.util.ArrayList;
+import java.util.HashMap;
+
+import edu.princeton.cs.algs4.Digraph;
+import edu.princeton.cs.algs4.DirectedCycle;
+import edu.princeton.cs.algs4.In;
 
 public class WordNet {
 
+	private HashMap<Integer, String> idToSynset = new HashMap<>();
+	private HashMap<String, ArrayList<Integer>> synonymToId = new HashMap<>();
+	private Digraph wordnetGraph;
+	private SAP shortestAncestralPath;
+
+	// constructor takes the name of the two input files
 	public WordNet(String synsets, String hypernyms) {
+		if (synsets == null || hypernyms == null) {
+			throw new IllegalArgumentException("File synsets or hypernyms cannot be null.");
+		}
+		In synsetsIn = new In(synsets);
+		In hypernymsIn = new In(hypernyms);
 
+		// Build synsets to ids.
+		while (!synsetsIn.isEmpty()) {
+			String[] values = synsetsIn.readLine().split(",");
+			int givenId = Integer.valueOf(values[0]);
+			String givenSynset = values[1];
+			idToSynset.put(givenId, givenSynset);
+			String[] synonyms = givenSynset.split(" ");
+
+			for (String synonym : synonyms) {
+				if (!synonymToId.containsKey(synonym)) {
+					synonymToId.put(synonym, new ArrayList<>());
+				}
+				synonymToId.get(synonym).add(givenId);
+			}
+		}
+
+		// Build wordnet digraph.
+		wordnetGraph = new Digraph(idToSynset.size());
+		while (!hypernymsIn.isEmpty()) {
+			String[] values = hypernymsIn.readLine().split(",");
+			int synset = Integer.valueOf(values[0]);
+
+			for (int i = 1; i < values.length; i++) {
+				wordnetGraph.addEdge(synset, Integer.valueOf(values[i]));
+			}
+		}
+
+		// Check invalid conditions.
+		if (new DirectedCycle(wordnetGraph).hasCycle()) {
+			throw new IllegalArgumentException("Wordnet graph cannot have a cycle.");
+		}
+
+		int graphRoots = 0;
+		for (int i = 0; i < wordnetGraph.V(); i++) {
+			if (!wordnetGraph.adj(i).iterator().hasNext()) {
+				graphRoots++;
+			}
+		}
+		if (graphRoots != 1) {
+			throw new IllegalArgumentException("Wordnet graph must have a single root.");
+		}
+
+		// Build shortest ancestral path.
+		shortestAncestralPath = new SAP(wordnetGraph);
 	}
 
-	public List<String> nouns() {
-		return new ArrayList<String>();
+	// returns all WordNet nouns
+	public Iterable<String> nouns() {
+		return synonymToId.keySet();
 	}
 
+	// is the word a WordNet noun?
 	public boolean isNoun(String word) {
-		return true;
+		if (word == null) {
+			throw new IllegalArgumentException("Given noun cannot be null.");
+		}
+		return synonymToId.containsKey(word);
 	}
 
+	// distance between nounA and nounB (defined below)
 	public int distance(String nounA, String nounB) {
-		return 0;
+		if (nounA == null || nounB == null || !isNoun(nounA) || !isNoun(nounB)) {
+			throw new IllegalArgumentException("Given nouns cannot be null.");
+		}
+		return shortestAncestralPath.length(synonymToId.get(nounA), synonymToId.get(nounB));
 	}
 
+	// a synset (second field of synsets.txt) that is the common ancestor of nounA and nounB
+	// in a shortest ancestral path (defined below)
 	public String sap(String nounA, String nounB) {
-		return "";
+		if (nounA == null || nounB == null || !isNoun(nounA) || !isNoun(nounB)) {
+			throw new IllegalArgumentException("Given nouns cannot be null.");
+		}
+		return idToSynset.get(shortestAncestralPath.ancestor(synonymToId.get(nounA), synonymToId.get(nounB)));
 	}
 
 	public static void main(String[] args) {
-
 	}
 
 }
