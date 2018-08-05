3b85314e4aab3987ced3e5e70e5f6e1c25610c70
roshan2M <r2munjal@edu.uwaterloo.ca>
2018-08-05 14:49:57 -0400

    Add testing client for SAP class.

--- src/week_1/problem_set/wordnet/SAP.java
+++ src/week_1/problem_set/wordnet/SAP.java
@@ -10,6 +10,9 @@ package week_1.problem_set.wordnet;
 
 import edu.princeton.cs.algs4.BreadthFirstDirectedPaths;
 import edu.princeton.cs.algs4.Digraph;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
 
 import java.util.ArrayList;
 
@@ -76,8 +79,18 @@ public class SAP {
 		return result[1];
 	}
 
-	// do unit testing of this class
+	// testing client for SAP
 	public static void main(String[] args) {
+		In in = new In(args[0]);
+		Digraph G = new Digraph(in);
+		SAP sap = new SAP(G);
+		while (!StdIn.isEmpty()) {
+			int v = StdIn.readInt();
+			int w = StdIn.readInt();
+			int length   = sap.length(v, w);
+			int ancestor = sap.ancestor(v, w);
+			StdOut.printf("length = %d, ancestor = %d\n", length, ancestor);
+		}
 	}
 
 }
