7d851eb94158661f4c41efa3cf0fcfe6a0b6eb4d
roshan2M <r2munjal@edu.uwaterloo.ca>
2018-08-11 12:46:41 -0400

    Find MBST by using the MST.

--- src/week_2/quizzes/MinimumSpanningTrees.java
+++ src/week_2/quizzes/MinimumSpanningTrees.java
@@ -5,6 +5,20 @@ import edu.princeton.cs.algs4.*;
 public class MinimumSpanningTrees {
 
 	/**
+	 * For finding the Minimum Bottleneck Spanning Tree (MBST),
+	 * just find the MST of the graph in O(E*log E) time. The MST
+	 * must be the MBST.
+	 *
+	 * Pf: Assume G is a weighted undirected graph with MST T and
+	 * T' is another spanning tree. Assume there exists edge e in T
+	 * such that e.weight() > f.weight() for all edges f in T'.
+	 *
+	 * Then adding e joins v, w in T' and yields a cycle C. However,
+	 * by the cut property, there is a shorter crossing between v, w,
+	 * contradicting that T is an MST.
+	 */
+
+	/**
 	 *
 	 * @param G is an edge-weighted graph
 	 * @param e is an edge in G
